56ff33e74013a7797407c4235d9707e7428a9583
Wan Ismat <ismatazmy@live.com>
2021-11-15 10:58:27 +0800

    + vue-router, popperjs & include bootstrap

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col">
+            This is some text within a card body.
+          </div>
+          <div class="col">
+            This is some text within a card body.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
